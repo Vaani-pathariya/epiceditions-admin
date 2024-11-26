b4b42e4fc030724ef598ed43d91e0cf18341b10f
Vaani <vaanipathariya@gmail.com>
2024-11-26 16:16:18 +0530

    (fix): Minor changes resolve

--- src/app/api/admin/login/route.ts
+++ src/app/api/admin/login/route.ts
@@ -2,7 +2,6 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { error } from "console";
 import jwt from "jsonwebtoken"
 connect();
 export async function POST(request: NextRequest){
